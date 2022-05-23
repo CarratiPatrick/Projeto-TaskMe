3e9e472e3f11de91797b404200aba31a05b46892
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-05-23 12:51:54 -0300

    model-contato completo

---
+++ js/controller/controller-contatos.js
@@ -0,0 +1,15 @@
+class Controlar {
+    adcionaMensagem() {
+      const nome = 
+      const email =
+      const msg =
+      const assunto =
+  
+      const model = new Anotacoes(titulo, nota);
+      model.verificaNota(titulo, nota);
+    }
+  }
+  
+  const controlar = new Controlar();
+  controlar.adcionaMensagem();
+  
