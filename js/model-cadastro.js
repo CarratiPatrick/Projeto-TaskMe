0ef691dca70c2e8410a79ae2dd8f97a97ac9975f
Josirocha <josi1992@gmail.com>
2022-05-24 11:55:29 -0300

    criação da class CadastroModel

---
+++ js/model-cadastro.js
@@ -0,0 +1,20 @@
+class CadastroModel {
+  constructor() {
+    this.nome = ""
+    this.sobrenome = ""
+    this.rg = ""
+    this.orgaoEmissor = ""
+    this.cep = ""
+    this.rua = ""
+    this.bairro = ""
+    this.cidade = ""
+    this.uf = ""
+    this.complemento = ""
+    this.numero = ""
+    this.email = ""
+    this.confirmaEmail = ""
+    this.senha = ""
+    this.confirmaSenha = ""
+
+  }
+}
