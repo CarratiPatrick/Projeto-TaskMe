ac7e69b62b0d46828dea46e5619c18c71cacc550
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-05-25 11:37:51 -0300

    Acertando os scripts e links do projeto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$("#btnLog").click((event)=>{
+    event.preventDefault();
+    
+    location.replace("/Html/login.html")
+
+  })
+  $("#btnCad").click((event)=>{
+    event.preventDefault();
+    
+    location.replace("/Html/cadastro.html")
+
+  })
