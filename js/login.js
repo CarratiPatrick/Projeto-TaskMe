274a3ced314e460a9ac7f6a044dc883d2025ef42
Liliana1609 <lily.webdev22@gmail.com>
2022-05-24 22:02:50 -0400

    Alteração no codigo do botão de login

--- js/login.js
+++ js/login.js
@@ -1,14 +1,23 @@
-function validaEmail() {
-  const email = document.querySelector("#email").value;
+let botao = document.getElementById("submit")
 
-  if (email.indexOf("@") > -1) {
-    const senha = document.querySelector("#password").value;
-    const tamanhoSenha = senha.lenght;
-    if (email.indexOf("@") > -1 && tamanhoSenha > 3) {
-      console.log("validei email e senha");
-    }
-    window.location.href = "./index.html";
-  } else {
+botao.addEventListener("click", (event) => {
+  event.preventDefault()
+  validaEmail()
+})
+function validaEmail() {
+  const email = document.querySelector("#email");
+  const senha = document.querySelector("#password").value;
+  const tamanhoSenha = senha.length;
+
+    if (email.value.indexOf("@") > -1 ) {
+      console.log("validei email");
+      if(tamanhoSenha > 3){
+        console.log("validei senha");
+        location.replace("../index.html");
+
+      }
+
+    }else {
     const p = document.querySelector("#resposta");
     p.innerText = "E-mail inválido!";
     p.style.color = "#ff0000";
