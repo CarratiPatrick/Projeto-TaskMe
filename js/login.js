e02163290b0a3f50a504b6553e6183379ccc1b6d
Liliana1609 <lily.webdev22@gmail.com>
2022-05-24 20:32:35 -0400

    Mostrar e ocultar senha do formulario de login

--- js/login.js
+++ js/login.js
@@ -14,3 +14,18 @@ function validaEmail() {
     p.style.color = "#ff0000";
   }
 }
+//Mostrar e ocultar senha
+const container = document.querySelector("#container");
+const senha = document.querySelector("#password");
+const icon = document.querySelector("#eyeSvg");
+
+icon.addEventListener("click", function () {
+  container.classList.toggle("visible");
+  if (container.classList.contains("visible")) {
+    icon.src = "./img/fechado.svg";
+    senha.type = "text";
+  } else {
+    icon.src = "./img/aberto.svg";
+    senha.type = "password";
+  }
+});
